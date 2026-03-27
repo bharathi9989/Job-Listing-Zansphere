9e9467850a5f282aa75cf5dfeb76af04ecbe8ca2
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-27 17:58:23 +0530

    refact : every renders  filter runs again so we used useMemo for filter logic

--- job-board/src/pages/Home.jsx
+++ job-board/src/pages/Home.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react"
+import { useState } from "react";
 import useDebounce from "../hooks/useDebounce";
 import { jobs } from "../data/jobs";
 import SearchBar from "../components/SearchBar";
 import Filters from "../components/Filters";
 import JobCard from "../components/JobCard";
 import Pagination from "../components/Pagination";
-
+import { useMemo } from "react";
 
 const Home = () => {
   const [search, setSearch] = useState("");
@@ -18,14 +18,16 @@ const Home = () => {
 
   // Filter Logic
 
-  const filteredJobs = jobs.filter((job) => {
-    return (
-      job.title.toLowerCase().includes(debouncedSearch.toLowerCase()) &&
-      (role ? job.role === role : true) &&
-      (type ? job.type === type : true) &&
-      (salary ? job.salary >= Number(salary) : true)
-    );
-  });
+ const filteredJobs = useMemo(() => {
+   return jobs.filter((job) => {
+     return (
+       job.title.toLowerCase().includes(debouncedSearch.toLowerCase()) &&
+       (role ? job.role === role : true) &&
+       (type ? job.type === type : true) &&
+       (salary ? job.salary >= Number(salary) : true)
+     );
+   });
+ }, [debouncedSearch, role, type, salary]);
 
   // 🔥 PAGINATION
   const itemsPerPage = 5;
@@ -34,27 +36,27 @@ const Home = () => {
     startIndex,
     startIndex + itemsPerPage,
   );
-    
-    return (
-      <div>
-        <h1>Job Board</h1>
 
-        <SearchBar search={search} setSearch={setSearch} />
+  return (
+    <div>
+      <h1>Job Board</h1>
 
-        <Filters setRole={setRole} setType={setType} setSalary={setSalary} />
+      <SearchBar search={search} setSearch={setSearch} />
 
-        {paginatedJobs.map((job) => (
-          <JobCard key={job.id} job={job} />
-        ))}
+      <Filters setRole={setRole} setType={setType} setSalary={setSalary} />
 
-        <Pagination
-          total={filteredJobs.length}
-          perPage={itemsPerPage}
-          currentPage={currentPage}
-          setCurrentPage={setCurrentPage}
-        />
-      </div>
-    );
+      {paginatedJobs.map((job) => (
+        <JobCard key={job.id} job={job} />
+      ))}
+
+      <Pagination
+        total={filteredJobs.length}
+        perPage={itemsPerPage}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+    </div>
+  );
 };
 
 export default Home;
