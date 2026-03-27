b0059d1585dfd7ae3496959370bcf1db25aa5f94
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-27 19:34:11 +0530

    fix : tailwind css import issue is fixed

--- job-board/src/components/Filters.jsx
+++ job-board/src/components/Filters.jsx
@@ -39,7 +39,7 @@ const Filters = ({ setRole, setType, setSalary, setSort }) => {
       </select>
 
       <button
-        className="col-span-2 md:col-span-4 bg-red-500 text-white py-2 rounded"
+        className="col-span-2 md:col-span-4 bg-red-500 text-white py-2 rounded hover:bg-red-600 transition duration-200"
         onClick={() => {
           setRole("");
           setType("");
