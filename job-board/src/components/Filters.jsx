9dc96e6003d3752041327b1c73e3b91ab34d2399
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-27 17:21:02 +0530

    feat : added a filter component in projects

---
+++ job-board/src/components/Filters.jsx
@@ -0,0 +1,26 @@
+const Filters = ({ setRole, setType, setSalary }) => {
+  return (
+    <div>
+      <select onChange={(e) => setRole(e.target.value)}>
+        <option value="">All Roles</option>
+        <option value="Frontend">Frontend</option>
+        <option value="Backend">Backend</option>
+        <option value="Fullstack">Fullstack</option>
+      </select>
+
+      <select onChange={(e) => setType(e.target.value)}>
+        <option value="">All Types</option>
+        <option value="Remote">Remote</option>
+        <option value="Onsite">Onsite</option>
+      </select>
+
+      <select onChange={(e) => setSalary(e.target.value)}>
+        <option value="">All Salaries</option>
+        <option value="500000">Above 5 LPA</option>
+        <option value="800000">Above 8 LPA</option>
+      </select>
+    </div>
+  );
+};
+
+export default Filters;
