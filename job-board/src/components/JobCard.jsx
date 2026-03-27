f821e669080b946d31f9c076f0bc158c90dfe4dd
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-27 23:12:09 +0530

    refact : restructure file of pagination

--- job-board/src/components/JobCard.jsx
+++ job-board/src/components/JobCard.jsx
@@ -1,14 +1,14 @@
-const JobCard = ({ job}) => { 
-    return (
-      <div className="border rounded-lg p-4 shadow-sm hover:shadow-md transition">
-        <h3 className="text-xl font-semibold">{job.title}</h3>
-        <p className="text-gray-600">Role: {job.role}</p>
-        <p className="text-gray-600">Type: {job.type}</p>
-        <p className="text-blue-600 font-bold mt-2">
-          ₹{job.salary.toLocaleString()}
-        </p>
-      </div>
-    );
-}
+const JobCard = ({ job }) => {
+  return (
+    <div className="border rounded-lg p-4 shadow-sm hover:shadow-md transition">
+      <h3 className="text-xl font-semibold">{job.title}</h3>
+      <p className="text-gray-600">Role: {job.role}</p>
+      <p className="text-gray-600">Type: {job.type}</p>
+      <p className="text-blue-600 font-bold mt-2">
+        ₹{job.salary.toLocaleString()}
+      </p>
+    </div>
+  );
+};
 
 export default JobCard;
--- job-board/src/components/Pagination.jsx
+++ job-board/src/components/Pagination.jsx
@@ -1,16 +1,11 @@
 const Pagination = ({ total, perPage, currentPage, setCurrentPage }) => {
   const totalPages = Math.ceil(total / perPage);
 
-  const itemsPerPage = 5;
-  const start = (currentPage - 1) * itemsPerPage;
-
-  const paginatedJobs = jobsData.slice(start, start + itemsPerPage);
-
   return (
     <div className="flex gap-2">
       <button
         disabled={currentPage === 1}
-        className="px-3 py-1 border rounded disabled:opacity-50"
+        className="px-3 py-1 border rounded disabled:opacity-50 cursor-pointer"
         onClick={() => setCurrentPage((p) => p - 1)}
       >
         Prev
@@ -19,7 +14,7 @@ const Pagination = ({ total, perPage, currentPage, setCurrentPage }) => {
       {[...Array(totalPages)].map((_, i) => (
         <button
           key={i}
-          className={`px-3 py-1 border rounded ${
+          className={`px-3 py-1 border rounded cursor-pointer ${
             currentPage === i + 1 ? "bg-blue-500 text-white" : ""
           }`}
           onClick={() => setCurrentPage(i + 1)}
@@ -30,18 +25,11 @@ const Pagination = ({ total, perPage, currentPage, setCurrentPage }) => {
 
       <button
         disabled={currentPage === totalPages}
-        className="px-3 py-1 border rounded disabled:opacity-50"
+        className="px-3 py-1 border rounded disabled:opacity-50 cursor-pointer"
         onClick={() => setCurrentPage((p) => p + 1)}
       >
         Next
       </button>
-      {loading ? (
-        <p>Loading...</p>
-      ) : paginatedJobs.length === 0 ? (
-        <p>No jobs found</p>
-      ) : (
-        paginatedJobs.map((job) => <JobCard key={job.id} job={job} />)
-      )}
     </div>
   );
 };
