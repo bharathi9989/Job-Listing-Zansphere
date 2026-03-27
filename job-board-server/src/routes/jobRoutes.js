1137634191457ab7d373522a1883d7cffe20d49c
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-27 22:55:33 +0530

    feat : initial comit for backend

---
+++ job-board-server/src/routes/jobRoutes.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { jobs } from "../data/jobs.js";
+
+const router = express.Router();
+
+// GET /api/jobs
+router.get("/", (req, res) => {
+  const { search, role, type, salary, sort } = req.query;
+
+  let result = [...jobs];
+
+  if (search) {
+    result = result.filter((job) =>
+      job.title.toLowerCase().includes(search.toLowerCase()),
+    );
+  }
+
+  if (role) result = result.filter((job) => job.role === role);
+  if (type) result = result.filter((job) => job.type === type);
+  if (salary) result = result.filter((job) => job.salary >= Number(salary));
+
+  if (sort === "low") result.sort((a, b) => a.salary - b.salary);
+  if (sort === "high") result.sort((a, b) => b.salary - a.salary);
+
+  res.json(result);
+});
+
+export default router;
