2ca4460962ab073b7056d171ef33c39662e311db
bharathi9989 <er.velubharathi.s@gmail.com>
2026-03-27 19:28:25 +0530

    feat : added a styling in JobCard

--- job-board/src/components/JobCard.jsx
+++ job-board/src/components/JobCard.jsx
@@ -1,10 +1,12 @@
 const JobCard = ({ job}) => { 
     return (
-      <div className="job-card">
-        <h3>{job.title}</h3>
-        <p>Role: {job.role}</p>
-        <p>Type: {job.type}</p>
-        <p>Salary: ₹{job.salary}</p>
+      <div className="border rounded-lg p-4 shadow-sm hover:shadow-md transition">
+        <h3 className="text-xl font-semibold">{job.title}</h3>
+        <p className="text-gray-600">Role: {job.role}</p>
+        <p className="text-gray-600">Type: {job.type}</p>
+        <p className="text-blue-600 font-bold mt-2">
+          ₹{job.salary.toLocaleString()}
+        </p>
       </div>
     );
 }
